791a939c5d369764aea66ea4c7761ce90ee02d59
Sakib Sami <s4kibs4mi@gmail.com>
2014-07-08 17:42:27 +0600

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,66 @@
+/**
+            BISMILLAHIR RAHMANIR RAHIM { IN THE NAME OF ALLAH }
+
+           LLLLLLL      LL     LL   LL    LL   LL    LLLLLLLLLL
+         LL        LL   LL     LL   LL   LL    LL    LL        LL
+         LL             LL     LL   LL  LL     LL    LL        LL
+           LLLLLL       LLLLLLLLL   LLL        LL    LLLLLLLLLL
+                  LLL          LL   LL  LL     LL    LL        LL
+        LL        LLL          LL   LL   LL    LL    LL        LL
+          LLLLLLLLL            LL   LL    LL   LL    LLLLLLLLLLL
+**/
+
+
+// All standard Headers
+#include <algorithm>
+#include <cctype>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <list>
+#include <map>
+#include <math.h>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// Data type
+typedef long long int LLI;
+typedef long int  LI;
+typedef vector<LLI> VLLI;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef map<int,int> MII;
+typedef map<string,int> MSI;
+typedef map<string,string> MSS;
+
+// I/O Defination
+#define SI(n) scanf("%d",&n)
+#define SIL(n) scanf("%lld",&n);
+#define SC(a) scanf("%c",&a)
+#define SS(x) scanf("%s",x)
+#define CPS(s) getline(cin , s)
+#define IP(n) printf("%d",n)
+#define ILP(n) printf("%lld",n)
+#define CPSP(a) cout << a << "\n"
+#define PB push_back
+#define MP make_pair
+#define MSET(a,v) memset(a,v,sizeof(a))
+#define STB(const) const.begin()
+#define STE(const) const.end()
+
+// Program Flow & Mathematical Functions
+#define FOR(i,n,inc) for(;i < n;i += inc)
+#define GCD(a,b) __gcd(a, b)
+#define LCM(a,b) (a/GCD(a,b))*b
+#define PI 3.1415926535897932384626433832795
+
+int main(){
+
+    return 0;
+}
